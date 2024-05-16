70415b09f77a62d60ef92b32460a72d87c864477
youfun <junezpe@gmail.com>
2024-05-16 13:43:36 +0800

    Update diy-hello.sh

--- diy-hello.sh
+++ diy-hello.sh
@@ -1,2 +1,7 @@
 sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+
+./scripts/feeds update helloworld
+./scripts/feeds install -a -f -p helloworld
+
+rm -fr bin/ dl/ staging_dir/ build_dir/ tmp/
